c16fadfd90bced7d7b7b018b21ab205ca9153d1b
Nalendrooo <nalendroadil274@gmail.com>
2022-10-04 14:38:05 +0700

    edited

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/asset/css/stylebuku.css">
-    <link rel="shortcut icon" href="<?php echo base_url() ?>assets/img/favicon.png">
+    <link rel="shortcut icon" href="<?php echo base_url() ?> asset/img/favicon.png">
 </head>
 
 <body>
